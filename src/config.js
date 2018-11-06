8733cc4cff666e804032957e3308dfe5643c70f2
夜宴 <1366792281@qq.com>
2018-11-06 17:58:54 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
- const BASE_URL =  process.env.NODE_ENV === 'development' ? 'http://www.baidu.com' : ' https://easy-mock.com/mock/5bcd28e5843f0a4819f792ae/example' //mock接口
- // const BASE_URL =  process.env.NODE_ENV === 'development' ? 'http://www.baidu.com' : ' https://localhost:3000' //联调接口
+ export const BASE_URL =  process.env.NODE_ENV === 'development' ? 'http://www.baidu.com' : ' https://easy-mock.com/mock/5bcd28e5843f0a4819f792ae/example' //mock接口
+ //export const BASE_URL =  process.env.NODE_ENV === 'development' ? 'http://www.baidu.com' : ' https://localhost:3000' //联调接口
 
