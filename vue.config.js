d2698c627c575a50afcbe4917620d84f5a4de1d1
yeyan1996 <1366792281@qq.com>
2019-01-08 16:52:03 +0800

    Revert "feat:webpack打包开启gzip,打包体积大幅减少"
    This reverts commit 83334bf19d092636e037d5ddeef59aecaab77165.

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,3 @@
-const CompressionWebpackPlugin=require('compression-webpack-plugin')
 const path = require('path')
 const IS_PRODUCTION = process.env.NODE_ENV === 'production'
 const IS_DEVELOPMENT = process.env.NODE_ENV === 'development'
@@ -8,10 +7,9 @@ const cdn = {
             "https://cdn.bootcss.com/element-ui/2.4.11/theme-chalk/index.css"
         ],
     js: [
-        "https://unpkg.com/vue@2.5.21/dist/vue.js",
+        "https://cdn.jsdelivr.net/npm/vue@2.5.21/dist/vue.js",
         "https://cdn.bootcss.com/vue-router/3.0.2/vue-router.min.js",
         "https://cdn.bootcss.com/element-ui/2.4.11/index.js",
-        // "https://unpkg.com/pdfjs-dist@2.0.943/lib/pdf.js",
     ]
 }
 
@@ -31,17 +29,6 @@ module.exports = {
                 .tap(args => {
                     args[0].cdn = cdn;
                     return args;
-                })
-                .end()
-                //gzip需要nginx进行配合否则无法使用
-                .plugin('compression')
-                .use(CompressionWebpackPlugin)
-                .tap(args => {
-                    return [{
-                        test: /\.js$|\.html$|\.css/, //匹配文件名
-                        threshold: 10240, //超过10k进行压缩
-                        deleteOriginalAssets: false //是否删除源文件
-                    }]
                 })
         }
         config.resolve.alias
