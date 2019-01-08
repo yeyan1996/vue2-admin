ed015a7924962906c2b5375cf58aebd22a48676d
yeyan1996 <1366792281@qq.com>
2019-01-08 11:36:38 +0800

    feat;表格组件添加process预处理表格字段,配置项添加了class

--- src/components/ZComponents/ZTable/index.vue
+++ src/components/ZComponents/ZTable/index.vue
@@ -1,19 +1,19 @@
 <!--需要使用组件实例ref属性只能在父组件定义ref属性，使用this.$refs.<父组件ref属性名>.$children[0]获取-->
 <template>
     <el-table
-            empty-text="没有符合条件的数据"
+            :empty-text="emptyText"
             v-bind="$attrs"
             v-on="$listeners"
             :ref="_ref"
             :data="data"
-            :stripe="$attrs.stripe!==false">
+            :stripe="stripe">
 
         <slot name="font" />
 
         <template v-for="(column,index) in columns" v-if="!column.hidden">
 
             <el-table-column
-                    v-if="!(column.options || column.operations || column.slot)"
+                    v-if="isCommonTableColumn(column)"
                     :key="index"
                     v-bind="column.attrs || {}">
             </el-table-column>
@@ -25,7 +25,7 @@
                 <template slot-scope="scope">
 
                     <div v-if="column.options">
-                        <span>{{key2name(scope.row[column.attrs.prop],column.options)}}</span>
+                        <span>{{value2name(scope.row[column.attrs.prop],column)}}</span>
                     </div>
 
                     <span v-else-if="column.slot">
@@ -34,28 +34,33 @@
 
                     <div v-else-if="column.operations">
                         <template v-for="operation in column.operations">
-                                <el-tooltip
-                                        v-if="operation.name"
-                                        effect="light"
+                            <el-tooltip
+                                    v-if="operation.name"
+                                    effect="light"
+                                    :key="operation.svgName"
+                                    :content="operation.name"
+                                    placement="top-end">
+                                <svg-icon
                                         :key="operation.svgName"
-                                        :content="operation.name"
-                                        placement="top-end">
-                                    <svg-icon
-                                            :key="operation.svgName"
-                                            class="icon"
-                                            :name="operation.svgName"
-                                            @click.native="handleOperation(operation.event,scope.row)">
-                                    </svg-icon>
-                                </el-tooltip>
+                                        :class="column.attrs.class"
+                                        :name="operation.svgName"
+                                        @click.native="handleOperation(operation.event,scope.row)">
+                                </svg-icon>
+                            </el-tooltip>
 
                             <svg-icon
                                     v-else
                                     :key="operation.svgName"
-                                    class="icon"
+                                    :class="column.attrs.class"
                                     :name="operation.svgName"
                                     @click.native="handleOperation(operation.event,scope.row)">
                             </svg-icon>
                         </template>
+                    </div>
+
+                    <!--需要进行process处理后的字段-->
+                    <div v-else-if="column.process">
+                        <span>{{column.process(scope.row[column.attrs.prop])}}</span>
                     </div>
 
                     <div v-else>未知表头</div>
@@ -79,23 +84,44 @@
                 required: true
             },
             data:{
-                // type:Array,
-                required:true
+                required:true,
+                default: () => []
             },
-            _ref:{
-                type:String
+            _ref: {
+                type: String
             }
         },
         methods: {
-            key2name(key, options) {
-                let option = options.find(option => key === option.key)
-                let defaultOption = options.find(option => option.key === 'default')
-                return option ? option.name : (defaultOption ? defaultOption.name : null)
+            // 是否是一个常规的table-column(有以下标签就不是常规table-column)
+            isCommonTableColumn({options,operations,slot,process}) {
+                return !(options || operations || slot || process)
+            },
+            // 找value对应的name
+            value2name(value, column) {
+                let option;
+                let defaultOption;
+                option = column.process ? this.processValue(value,column) : column.options.find(option => value === option.key)
+                // 是否在没有找到name的时候使用默认name
+                defaultOption = column.options.find(option => option.key === 'default')
+                return option ? option.name : (defaultOption && defaultOption.name)
+            },
+            // 需要对tableData的字段进行预处理的情况
+            processValue(value,column){
+                return column.options.find(option => value == null || column.process(value) === option.key)
             },
             handleOperation(event, row) {
                 this.$emit(event, row)
             },
+        },
+        computed: {
+            emptyText() {
+                return this.$attrs['empty-text'] || '没有符合条件的数据'
+            },
+            stripe() {
+                return this.$attrs.stripe !== false
+            }
         }
     }
 </script>
+
 
--- src/interface/Columns.ts
+++ src/interface/Columns.ts
@@ -2,8 +2,9 @@ export interface Columns {
     attrs?: Attrs, //所有传入el-table-column属性的对象
     hidden?:boolean //显示/隐藏
     slot?:string, /**插槽,特别复杂的交互通过作用域插槽交给父组件**/
-    options?: Options[], //用于key2name的对象
+    options?: Options[] , //用于key2name的对象
     operations?:Operations[],  //用于控制图标功能的对象
+    process?(tableDataValue:string):string //预处理表格字段的函数
 }
 
 interface Attrs {
@@ -16,6 +17,7 @@ interface Options {
     key: string | number, //字段名
     name: string //中文名
 }
+
 
 interface Operations{
     svgName:string,  //svg的图标名
