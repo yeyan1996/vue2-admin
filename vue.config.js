fcd0198d9a0ea72ecb237ae29c38f73e6701cc3e
yeyan1996 <1366792281@qq.com>
2019-01-09 17:21:16 +0800

    update:调整了chainWebpack的结构,删除了vuex的cdn,调整了index.html的结构

--- vue.config.js
+++ vue.config.js
@@ -1,46 +1,27 @@
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const UglifyjsWebpackPlugin = require('uglifyjs-webpack-plugin')
 const CompressionWebpackPlugin = require('compression-webpack-plugin')
+const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer')
 const path = require('path')
 const IS_PRODUCTION = process.env.NODE_ENV === 'production'
 const IS_DEVELOPMENT = process.env.NODE_ENV === 'development'
 const cdn = [
-        "https://cdn.bootcss.com/vue/2.5.21/vue.min.js",
-        "https://cdn.bootcss.com/vue-router/3.0.2/vue-router.min.js",
-        "https://cdn.bootcss.com/vuex/3.0.1/vuex.min.js",
-        "https://cdn.bootcss.com/element-ui/2.4.11/index.js"
-    ]
+    "https://cdn.bootcss.com/vue/2.5.21/vue.min.js",
+    "https://cdn.bootcss.com/vue-router/3.0.2/vue-router.min.js",
+    "https://cdn.bootcss.com/element-ui/2.4.11/index.js",
+]
 
-function resolve (dir) {
+function resolve(dir) {
     return path.join(__dirname, dir)
 }
 
+
 module.exports = {
-    baseUrl:'',
+    baseUrl: '',
+    assetsDir: './static',
     chainWebpack: config => {
-        if(IS_PRODUCTION){
-            config
-                .plugin('webpack-bundle-analyzer')
-                .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin)
-                .end()
-                .plugin('html')
-                .tap(args => {
-                    args[0].cdn = cdn;
-                    return args;
-                })
-                //gzip需要nginx进行配合否则无法使用
-                .plugin('compression')
-                .use(CompressionWebpackPlugin)
-                .tap(args => {
-                    return [{
-                        test: /\.js$|\.html$|\.css/, //匹配文件名
-                        threshold: 10240, //超过10k进行压缩
-                        deleteOriginalAssets: false //是否删除源文件
-                    }]
-                })
-        }
         config.resolve.alias
             .set('@', resolve('src'))
-            .set('util',resolve('src/util'))
+            .set('util', resolve('src/util'))
         const svgRule = config.module.rule('svg')
         svgRule.uses.clear()
         config.module
@@ -54,28 +35,38 @@ module.exports = {
             .options({
                 symbolId: 'icon-[name]'
             })
-    },
-    css: {
-        loaderOptions: {
-            // 给 sass-loader 传递选项
-            sass: {
-                // @/ 是 src/ 的别名
-                // 所以这里假设你有 `src/variables.scss` 这个文件
-                data: `@import "@/style/index.scss";`
-            }
+        if (IS_PRODUCTION) {
+            config
+                .plugin('analyzer')
+                .use(BundleAnalyzerPlugin)
+                .end()
+                .plugin('html')
+                .tap(args => {
+                    args[0].cdn = cdn;
+                    return args;
+                })
+                .end()
+                //gzip需要nginx进行配合否则无法使用
+                .plugin('compression')
+                .use(CompressionWebpackPlugin)
+                .tap(() => [{
+                        test: /\.js$|\.html$|\.css/, //匹配文件名
+                        threshold: 10240, //超过10k进行压缩
+                        deleteOriginalAssets: false //是否删除源文件
+                    }]
+                )
+            config.externals({
+                'vue': 'Vue',
+                'vue-router': 'VueRouter',
+                'element-ui': 'ELEMENT',
+            })
         }
     },
     configureWebpack: config => {
         if (IS_PRODUCTION) {
-            config.externals = {
-                'vue': 'Vue',
-                'vue-router': 'VueRouter',
-                'vuex':'vuex',
-                'element-ui': 'ELEMENT',
-            }
             config.optimization = {
                 minimizer: [
-                    new UglifyJsPlugin({
+                    new UglifyjsWebpackPlugin({
                         uglifyOptions: {
                             compress: {
                                 warnings: false,
@@ -89,9 +80,20 @@ module.exports = {
             }
         }
     },
+    css: {
+        loaderOptions: {
+            // 给 sass-loader 传递选项
+            sass: {
+                // @/ 是 src/ 的别名
+                // 所以这里假设你有 `src/variables.scss` 这个文件
+                data: `@import "@/style/index.scss";`
+            }
+        }
+    },
     devServer: {
         host: '0.0.0.0',
-        overlay:false,
+        overlay: false,
         port: 8000,
-    },
+        open: true
+    }
 }
