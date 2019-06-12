2cb066c747126b757d65572777deb72fa56c5840
yeyan1996 <1366792281@qq.com>
2019-06-12 15:17:29 +0800

    feat:错误捕获添加 vuex 支持 update:使用npm 替换yarn fix:修复errorHandler无法正确显示的问题

--- src/plugins/errorHandler/index.js
+++ src/plugins/errorHandler/index.js
@@ -1,4 +1,5 @@
-const errorHandler = (vm, info) => err => {
+// 可以自定义错误逻辑，发送日志等
+const errorHandler = (err, vm, info) => {
   console.error(err);
   console.error(vm);
   console.error(info);
@@ -7,28 +8,57 @@ const errorHandler = (vm, info) => err => {
 const isPromise = promise =>
   Object.prototype.toString.call(promise) === "[object Promise]";
 
+let isRegistered = false;
+
+const registerVue = vm => {
+  let methods = vm.$options.methods || {};
+  Object.keys(methods).forEach(key => {
+    let method = methods[key];
+    methods[key] = function(...args) {
+      let res = method.call(vm, ...args);
+      return isPromise(res)
+        ? res.catch(err =>
+            errorHandler(
+              err,
+              vm,
+              `promise method error,method name:'${method.name}'`
+            )
+          )
+        : res;
+    };
+  });
+};
+
+const registerVuex = vm => {
+  if (isRegistered) return;
+  if (!vm.$store || !vm.$store._actions) return;
+  Object.keys(vm.$store._actions).forEach(key => {
+    vm.$store._actions[key] = vm.$store._actions[key].map(func => (...args) => {
+      let res = func.call(vm.$store, ...args);
+      return isPromise(res)
+        ? res.catch(err =>
+            errorHandler(
+              err,
+              vm.$store,
+              `vuex action error,action name:'${key}'`
+            )
+          )
+        : res;
+    });
+  });
+  // 防止多次注册导致性能问题
+  isRegistered = true;
+};
+
 // 捕获 methods 中 async 函数的错误，省去 try/catch
-// 不会捕获生命周期中的 async 函数错误，因为推荐将逻辑都写在 methods 中
+// 同时支持 vuex
 export default {
   install(Vue) {
     Vue.config.errorHandler = errorHandler;
     Vue.mixin({
       beforeCreate() {
-        let methods = this.$options.methods || {};
-        Object.keys(methods).forEach(key => {
-          let method = methods[key];
-          methods[key] = function(...args) {
-            let res = method.call(this, ...args);
-            return isPromise(res)
-              ? res.catch(
-                  errorHandler(
-                    this,
-                    `promise method error,method name:'${method.name}'`
-                  )
-                )
-              : res;
-          };
-        });
+        registerVuex(this);
+        registerVue(this);
       }
     });
   }
