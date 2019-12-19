a574af3adc7c149154bf70bcd6465f22654e5c1b
夜宴 <1366792281@qq.com>
2019-12-19 13:45:20 +0800

    Update .stylelintrc.js

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,6 +1,24 @@
 module.exports = {
   extends: "stylelint-config-standard",
   rules: {
-    "at-rule-no-unknown": null
+    "at-rule-no-unknown": null,
+    "at-rule-empty-line-before": [
+      "always",
+      {
+        ignoreAtRules: ["else"]
+      }
+    ],
+    "block-closing-brace-newline-after": [
+      "always",
+      {
+        ignoreAtRules: ["if", "else"]
+      }
+    ],
+    "selector-pseudo-element-no-unknown": [
+      true,
+      {
+        ignorePseudoElements: ["v-deep"]
+      }
+    ]
   }
 };
