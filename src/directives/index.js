ba5e090e42099a5b763cf7af1e6bf0aaf083519a
夜宴 <1366792281@qq.com>
2020-01-06 11:34:47 +0800

    Update index.js

--- src/directives/index.js
+++ src/directives/index.js
@@ -1,8 +1,15 @@
-import _debounce from "lodash/debounce";
+import { debounce as _debounce } from "lodash";
 const DEFAULT_TIME = 3000;
+
 export const debounce = {
-  bind(el, { arg, modifiers, value }) {
-    let duration = Number(Object.keys(modifiers)[0]) || DEFAULT_TIME;
-    el.addEventListener(arg, _debounce(value, duration));
+  bind(el, { arg, modifiers, value }, vnode) {
+    const isComponent = !!vnode.child;
+    const duration = Number(Object.keys(modifiers)[0]) || DEFAULT_TIME;
+
+    if (isComponent) {
+      vnode.child.$on(arg, _debounce(value, duration));
+    } else {
+      el.addEventListener(arg, _debounce(value, duration));
+    }
   }
 };
